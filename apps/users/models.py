3929fdedd9092dc4efd9b4941f8f4bfcf3a20c39
Saidulloh <ssavutokhunov@gmail.com>
2022-12-21 18:35:55 +0600

    create model User

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser 
 
-# Create your models here.
+from utils.NumberValidator import phone_validator
+
+
+class User(AbstractUser):
+    """
+    Model for user
+    """
+    username = models.CharField(
+        max_length=255,
+        unique=True,
+        verbose_name='username'
+    )
+    email = models.EmailField(
+        unique=True,
+        verbose_name='email'
+    )
+    phone_number = models.CharField(
+        max_length=13,
+        validators=[phone_validator],
+        verbose_name='phone_number'
+    )
+    create_at = models.DateTimeField( 
+        auto_now_add=True,
+        verbose_name='create_at'
+    )
+    age = models.IntegerField(
+        verbose_name='age'
+    )
+
+    def __str__(self):
+        return f'{self.id} -- {self.username}'
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'Users'
