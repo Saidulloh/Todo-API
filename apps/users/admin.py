2a2c14913ee0038164b53fdcb16f1ef4ed7aba06
Saidulloh <ssavutokhunov@gmail.com>
2022-12-21 19:02:21 +0600

    registered model User in admin panel

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.users.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'create_at')
+
+admin.site.register(User, UserAdmin)
