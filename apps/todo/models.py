cfe599383296b7cd59513962fdea966f80a8737f
Saidulloh <ssavutokhunov@gmail.com>
2022-12-22 16:29:15 +0600

    create model Todo

--- apps/todo/models.py
+++ apps/todo/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    """
+    Model for todo list
+    """
+    title = models.CharField(
+        max_length=256,
+        unique=True,
+        verbose_name='title'
+    )
+    description = models.TextField(
+        verbose_name='description'
+    )
+    is_completed = models.BooleanField(
+        default=False,
+        verbose_name='is comleted'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='created at'
+    )
+    image = models.ImageField(
+        upload_to='todo_images/',
+        verbose_name='image'
+    )
+
+    def __str__(self):
+        return f'{self.id}'
+
+    class Meta:
+        verbose_name = 'todo'
+        verbose_name_plural = 'Todos'
