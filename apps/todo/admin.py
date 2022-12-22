3bdd6e246648bcc06be5d9f9feab37fe26f876ba
Saidulloh <ssavutokhunov@gmail.com>
2022-12-22 16:29:37 +0600

    registered model Todo in admin panel

--- apps/todo/admin.py
+++ apps/todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.todo.models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+admin.site.register(Todo, TodoAdmin)
