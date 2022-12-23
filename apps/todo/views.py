ba5b0c41f9582b02cb875b54e00c46e523547bb5
Saidulloh <ssavutokhunov@gmail.com>
2022-12-23 15:38:37 +0600

    create filter

--- apps/todo/views.py
+++ apps/todo/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView, DestroyAPIView, ListAPIView
 from rest_framework.response import Response
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from apps.todo.models import Todo
 from apps.todo.serializers import TodoSerializer
@@ -18,9 +19,34 @@ class TodoRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
 class TodoListCreateAPIView(ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    filter_backends = [SearchFilter, OrderingFilter]
+    filter_fields = [
+        'title',
+        'is_completed',
+        'created_at'
+    ]
+    ordering_fields = [
+        'title',
+        'is_completed',
+        'created_at'
+    ]
+    search_fields = [
+        'title',
+        '=created_at'
+    ]
 
     def get_queryset(self):
-        return Todo.objects.filter(owner_id=self.request.user.id)
+        title = self.request.query_params.get('title')
+        created_at = self.request.query_params.get('created_at')
+        if title and created_at:
+            queryset = Todo.objects.filter(owner_id=self.request.user.id, title=title, created_at=created_at)
+        elif title:
+            queryset = Todo.objects.filter(owner_id=self.request.user.id, title=title)
+        elif created_at:
+            queryset = Todo.objects.filter(owner_id=self.request.user.id, created_at=created_at)
+        else:
+            queryset = Todo.objects.filter(owner_id=self.request.user.id)
+        return queryset
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
