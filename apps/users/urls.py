7e9baa22e1b55048e5999dc937f9cbdcbc44b29f
Saidulloh <ssavutokhunov@gmail.com>
2022-12-23 22:55:43 +0600

    edit urls

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from apps.users.views import UserRetrieveUpdateDestroyAPIView, UserListAPIView, UserCreateAPIView
+from apps.users.views import UserRetrieveUpdateDestroyAPIView, UserListCreateAPIView
 
 
 urlpatterns = [
     path('<int:pk>/', UserRetrieveUpdateDestroyAPIView.as_view()),
-    path('', UserListAPIView.as_view()),
-    path('<int:pk>/', UserCreateAPIView.as_view()),
+    path('', UserListCreateAPIView.as_view()),
 ]
